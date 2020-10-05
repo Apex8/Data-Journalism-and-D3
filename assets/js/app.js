3da696f4f0922d548f41dd6c988334bb646de4d3
John J. Falcone <66309588+Apex8@users.noreply.github.com>
2020-10-05 14:46:10 -0400

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,14 @@
-// @TODO: YOUR CODE HERE!
+var svgWidth = 960;
+var svgHeight = 500;
+
+var margin = {
+  top: 20,
+  right: 40,
+  bottom: 80,
+  left: 100
+};
+
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
+var chart = d3.select("#scatter").append("div").classed("chart", true);
+
