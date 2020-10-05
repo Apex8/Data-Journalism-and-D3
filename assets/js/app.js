c87e21589e694a320aab147cd1fdea87f62f3176
John J. Falcone <66309588+Apex8@users.noreply.github.com>
2020-10-05 14:54:42 -0400

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -18,6 +18,10 @@ var svg = d3
   .attr("width", svgWidth)
   .attr("height", svgHeight);
 
-  var chartGroup = svg.append("g")
+var chartGroup = svg.append("g")
   .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
+d3.select("body")
+  .append("div")
+  .attr("class", "tooltip")
+  .style("opacity", 0);
