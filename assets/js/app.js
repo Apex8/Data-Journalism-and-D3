00b2523c0bb5e43cdbb622d7d4d03748b75d8907
John J. Falcone <66309588+Apex8@users.noreply.github.com>
2020-10-05 14:49:50 -0400

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,3 +12,12 @@ var width = svgWidth - margin.left - margin.right;
 var height = svgHeight - margin.top - margin.bottom;
 var chart = d3.select("#scatter").append("div").classed("chart", true);
 
+var svg = d3
+  .select("body")
+  .append("svg")
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
+
+  var chartGroup = svg.append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
